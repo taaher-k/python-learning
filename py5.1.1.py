08682f5f4db76a9fdb7e59a6828f92ed296230d1
taaher-k <taaher.rehmanii@gmail.com>
2025-11-11 22:40:31 +0530

    python unit 5.1.1 completed
    Fibonacci numbers in generator function

--- py5.1.1.py
+++ py5.1.1.py
@@ -5,7 +5,7 @@
 
 
 #1
-
+"""
 def fibonacci_up_to(n):
     a, b = 0, 1
     while a <= n:
@@ -17,3 +17,21 @@ limit = 100
 print(f"Fibonacci numbers up to {limit}:")
 for num in fibonacci_up_to(limit):
     print(num, end=" ")
+"""
+
+
+
+def generater_fun_fibo(x):
+    a,b = 0,1
+    while a <= x:
+        yield a
+        a,b = b,a+b
+
+
+limit = 100
+print(f"im printing fibona sec to the given limit:{limit}")
+for num in generater_fun_fibo(limit):
+
+    print(num,end=" ")
+
+
