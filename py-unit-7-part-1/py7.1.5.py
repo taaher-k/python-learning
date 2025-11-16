e8a08f5e96ba2d6868bcc3c4d00430a685c58cf0
taaher-k <taaher.rehmanii@gmail.com>
2025-11-16 20:04:24 +0530

    unit-7 part1  -m5 ex completed #Find the Factorial of a given number.

---
+++ py-unit-7-part-1/py7.1.5.py
@@ -0,0 +1,23 @@
+#5
+
+
+#Find the Factorial of a given number.
+
+
+def factorial(n):
+    if n < 0:
+        return "Factorial not defined for negative numbers"
+    elif n == 0 or n == 1:
+        return 1
+    else:
+        result = 1
+        for i in range(2, n+1):
+            result *= i
+        return result
+
+# Example
+num = int(input("Enter a number: "))
+print(f"Factorial of {num} is {factorial(num)}")
+
+
+
