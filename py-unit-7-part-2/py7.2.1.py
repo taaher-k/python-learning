5d201fb001ae400fad9c3436c91fedd2fc9380bd
taaher-k <taaher.rehmanii@gmail.com>
2025-11-16 20:14:56 +0530

    unit-7 part1  -m5 ex def zero_one_pattern(rows):

---
+++ py-unit-7-part-2/py7.2.1.py
@@ -0,0 +1,20 @@
+#1. Zeros/ ones Pattern Programs
+
+
+
+#1
+
+def zero_one_pattern(rows):
+    for i in range(1, rows+1):
+        pattern = ""
+        for j in range(1, i+1):
+            if j % 2 == 1:   # odd position → 1
+                pattern += "1"
+            else:            # even position → 0
+                pattern += "0"
+        print(pattern)
+
+# Example: 5 rows
+zero_one_pattern(5)
+
+
